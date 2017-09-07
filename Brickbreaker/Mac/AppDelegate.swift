35039029c4d72d2ddcf6f7a13a4f86b093ff0d2e
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2017-09-07 19:28:02 +0800

    Mostly legal stuff
    Added GPL text to source files
    Removed references to old source code
    Created SettingsController
    Linked interface elements to IBOutlets in SettingsController
    Added Credits.rtf
    Menu items trigger WindowController 'Show' segues

--- Brickbreaker/Mac/AppDelegate.swift
+++ Brickbreaker/Mac/AppDelegate.swift
@@ -3,74 +3,29 @@
 //  BrickBreaker Mac
 //
 //  Created by Alessandro Vinciguerra on 12/12/2016.
-//  Copyright © 2016 Arc676. All rights reserved.
-//
+//	<alesvinciguerra@gmail.com>
+//Copyright (C) 2016-7 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
 
 import Cocoa
 
 @NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
-
-    //UI
-    @IBOutlet weak var window: NSWindow!
-//    var view: GameView
-    @IBOutlet weak var helpWindow: NSPanel!
-    @IBOutlet weak var settingsWindow: NSWindow!
-
-    //game settings
-    //game modes and settings
-    //timed regen
-    @IBOutlet weak var enableTimeRegen: NSButton!
-    @IBOutlet weak var time: NSTextField!
-    //clearings regen
-    @IBOutlet weak var enableClearingsRegen: NSButton!
-    @IBOutlet weak var clearings: NSTextField!
-    //random color change
-    @IBOutlet weak var enableRandomColorChange: NSButton!
-    @IBOutlet weak var colorChangeTime: NSTextField!
-    //endlessness
-    @IBOutlet weak var timeLimit: NSTextField!
-    var endlessModeEnabled: Bool
-    //tile colors
-    @IBOutlet weak var tileColor1: NSColorWell!
-    @IBOutlet weak var tileColor2: NSColorWell!
-    @IBOutlet weak var tileColor3: NSColorWell!
-    @IBOutlet weak var tileColor4: NSColorWell!
-    //tile shape
-    @IBOutlet weak var tileShapeSelection: NSPopUpButton!
-    //arcade
-    @IBOutlet weak var enableArcadeMode: NSButton!
-    //background options
-    @IBOutlet weak var backgroundStyle: NSMatrix!
-    @IBOutlet weak var colorBGMode: NSButtonCell!
-    @IBOutlet weak var imageBGMode: NSButtonCell!
-    @IBOutlet weak var bgColor: NSColorWell!
-    @IBOutlet weak var bgImage: NSImageView!
-    //music settings
-    @IBOutlet weak var pathToMusic: NSPathControl!
-    @IBOutlet weak var loopMusic: NSButton!
-    var music: NSSound
-    //interface settings
-    @IBOutlet weak var quitOnClose: NSButton!
-    //high scores
-    @IBOutlet weak var pathToFile: NSPathControl!
+class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        endlessModeEnabled = false
-        window.delegate = self
-        timeLimit.isEnabled = false
         newGame(NSNull())
-    }
-
-    override init() {
-        music = NSSound()
-        endlessModeEnabled = false
-    }
-
-    func windowWillClose(_ notification: Notification) {
-        if quitOnClose.state == NSOnState {
-            NSApplication.shared().terminate(self)
-        }
     }
 
     @IBAction func newGame(_ sender: AnyObject) {
@@ -79,29 +34,5 @@ class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
 
 	@IBAction func saveScore(_ sender: Any) {
 	}
-	
-    @IBAction func showHelp(_ sender: AnyObject) {
-        helpWindow.setIsVisible(true)
-    }
-
-    @IBAction func showSettings(_ sender: AnyObject) {
-        settingsWindow.setIsVisible(true)
-    }
-
-    @IBAction func toggleEndless(_ sender: AnyObject) {
-        endlessModeEnabled = (sender.state == NSOnState)
-        timeLimit.isEnabled = !endlessModeEnabled
-    }
-
-    @IBAction func chooseMusic(_ sender: AnyObject) {
-        let panel = NSOpenPanel()
-        panel.allowedFileTypes = ["aiff","mp3","m4a","wav"]
-        panel.allowsMultipleSelection = false
-        if panel.runModal() == NSFileHandlingPanelOKButton {
-            pathToMusic.url = panel.url!
-            music = NSSound(contentsOf: panel.url!, byReference: true)!
-            music.loops = (loopMusic.state == NSOnState)
-        }
-    }
 
 }
---
+++ Brickbreaker/Mac/SettingsController.swift
@@ -0,0 +1,83 @@
+//
+//  SettingsController.swift
+//  Brickbreaker
+//
+//  Created by Alessandro Vinciguerra on 07/09/2017.
+//	<alesvinciguerra@gmail.com>
+//Copyright (C) 2017 Arc676/Alessandro Vinciguerra
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation (version 3)
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+
+//You should have received a copy of the GNU General Public License
+//along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//See README and LICENSE for more details
+
+import Cocoa
+
+class SettingsController: NSViewController {
+	
+	//game settings
+	//game modes and settings
+	//timed regen
+	@IBOutlet weak var enableTimeRegen: NSButton!
+	@IBOutlet weak var time: NSTextField!
+	//clearings regen
+	@IBOutlet weak var enableClearingsRegen: NSButton!
+	@IBOutlet weak var clearings: NSTextField!
+	//random color change
+	@IBOutlet weak var enableRandomColorChange: NSButton!
+	@IBOutlet weak var colorChangeTime: NSTextField!
+	//endlessness
+	@IBOutlet weak var timeLimit: NSTextField!
+	var endlessModeEnabled: Bool = false
+	//tile colors
+	@IBOutlet weak var tileColor1: NSColorWell!
+	@IBOutlet weak var tileColor2: NSColorWell!
+	@IBOutlet weak var tileColor3: NSColorWell!
+	@IBOutlet weak var tileColor4: NSColorWell!
+	//tile shape
+	@IBOutlet weak var tileShapeSelection: NSPopUpButton!
+	//arcade
+	@IBOutlet weak var enableArcadeMode: NSButton!
+	//background options
+	@IBOutlet weak var backgroundStyle: NSMatrix!
+	@IBOutlet weak var colorBGMode: NSButtonCell!
+	@IBOutlet weak var imageBGMode: NSButtonCell!
+	@IBOutlet weak var bgColor: NSColorWell!
+	@IBOutlet weak var bgImage: NSImageView!
+	//music settings
+	@IBOutlet weak var pathToMusic: NSPathControl!
+	@IBOutlet weak var loopMusic: NSButton!
+	var music: NSSound?
+	//interface settings
+	@IBOutlet weak var quitOnClose: NSButton!
+	//high scores
+	@IBOutlet weak var pathToFile: NSPathControl!
+
+	@IBAction func toggleEndless(_ sender: AnyObject) {
+		endlessModeEnabled = (sender.state == NSOnState)
+		timeLimit.isEnabled = !endlessModeEnabled
+	}
+
+	@IBAction func chooseMusic(_ sender: AnyObject) {
+		let panel = NSOpenPanel()
+		panel.allowedFileTypes = ["aiff","mp3","m4a","wav"]
+		panel.allowsMultipleSelection = false
+		if panel.runModal() == NSFileHandlingPanelOKButton {
+			pathToMusic.url = panel.url!
+			music = NSSound(contentsOf: panel.url!, byReference: true)
+			music!.loops = (loopMusic.state == NSOnState)
+		}
+	}
+
+	@IBAction func musicButton(_ sender: NSButton) {
+	}
+
+}
