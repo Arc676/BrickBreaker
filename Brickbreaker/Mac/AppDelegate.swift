0ecc0bc8fa92a5df4b19f6f92dbe74f19b127137
Arc676/Alessandro Vinciguerra <alesvinciguerra@gmail.com>
2018-04-01 10:44:08 +0800

    Updated copyright years

--- Brickbreaker/Mac/AppDelegate.swift
+++ Brickbreaker/Mac/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Alessandro Vinciguerra on 12/12/2016.
 //	<alesvinciguerra@gmail.com>
-//Copyright (C) 2016-7 Arc676/Alessandro Vinciguerra
+//Copyright (C) 2016-8 Arc676/Alessandro Vinciguerra
 
 //This program is free software: you can redistribute it and/or modify
 //it under the terms of the GNU General Public License as published by
